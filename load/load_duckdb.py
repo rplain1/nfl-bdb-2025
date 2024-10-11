ae6b1e987cabbf0f332b63d3644fab17fd6dd14a
Ryan Plain <ryan@Ryans-MBP.local>
2024-10-11 13:44:24 -0600

    nullstr='NA' on all loads

--- load/load_duckdb.py
+++ load/load_duckdb.py
@@ -16,28 +16,28 @@ con.execute(
     """
     DROP TABLE IF EXISTS player_play;
     CREATE TABLE player_play AS
-    SELECT * FROM read_csv_auto('raw-data/player_play.csv')
+    SELECT * FROM read_csv_auto('raw-data/player_play.csv', nullstr='NA')
 """
 )
 con.execute(
     """
     DROP TABLE IF EXISTS players;
     CREATE TABLE players AS
-    SELECT * FROM read_csv_auto('raw-data/players.csv')
+    SELECT * FROM read_csv_auto('raw-data/players.csv', nullstr='NA')
 """
 )
 con.execute(
     """
     DROP TABLE IF EXISTS plays;
     CREATE TABLE plays AS
-    SELECT * FROM read_csv_auto('raw-data/plays.csv')
+    SELECT * FROM read_csv_auto('raw-data/plays.csv', nullstr='NA')
 """
 )
 con.execute(
     """
     DROP TABLE IF EXISTS games;
     CREATE TABLE games AS
-    SELECT * FROM read_csv_auto('raw-data/games.csv')
+    SELECT * FROM read_csv_auto('raw-data/games.csv', nullstr='NA')
 """
 )
 
